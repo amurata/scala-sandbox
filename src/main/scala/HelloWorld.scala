1bb5c74853eab76adb54ff14b6161bbb0a882414
amurata <rikuma95@gmail.com>
2022-02-11 17:44:46 +0900

    Hello Scala

---
+++ src/main/scala/HelloWorld.scala
@@ -0,0 +1,19 @@
+// App trait を使用して、アプリケーションを定義する
+object HelloWorld extends App {
+  // プログラム引数はargs。args(0)とすると各要素にアクセスできる。
+  println("Hello, World!")
+}
+//object HelloWorld {
+//
+//  def main(args:Array[String]):Unit = {
+//    println("Hello, World!")
+//  }
+//
+//}
+
+// in Java
+//public class HelloWorld{
+//  public static void main(String[] args){
+//    System.out.println("Hello, World!");
+//  }
+//}
